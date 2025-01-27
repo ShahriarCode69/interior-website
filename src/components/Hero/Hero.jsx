6352ab87261a221d6fcb9017a1810770b1acb093
techShabaka Agency <shahriarcode69@gmail.com>
2025-01-27 23:37:38 +0600

    hero klk korbo abr

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,17 +4,15 @@ const Hero = () => {
   return (
     <>
       <div className="hero__container m-12 flex flex-col items-center justify-center">
-        <h1 className="font-bold mt-10 text-4xl text-center">SKETCH LUXURY INTERIOR DESIGN</h1>
-        <p className="mt-3 text-center ">
+        <h1 className="font-bold mt-10 text-4xl text-center">
+          SKETCH LUXURY INTERIOR DESIGN
+        </h1>
+        <p className="mt-3 text-center leading-5 text-base">
           Bring your dream to life with one-on-one design help & hand picked
           products tailored to your style, space and budget.
         </p>
-        <button>
-          <a
-            href="#"
-            className="text-base transform hover:translate-y-[-0.4rem] transition duration-300 ease-in-out bg-black text-white p-3 cursor-pointer">
-            Get Started
-          </a>
+        <button className="m-5 text-base transform hover:translate-y-[-0.4rem] transition duration-300 ease-in-out bg-black text-white p-3 cursor-pointer">
+          <a href="#">Get Started</a>
         </button>
       </div>
     </>
