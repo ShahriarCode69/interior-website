98c58b30063efd514bf4f6f6ed9075b15d65fc86
techShabaka Agency <shahriarcode69@gmail.com>
2025-02-06 15:58:22 +0600

    project will be vanilla css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import './utils.css'
 
 export const App = () => {
   return (
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "./Hero.css";
 const Hero = () => {
   return (
     <>
-      <div className="hero__content">
+      <div className="container section hero__content">
         <h1 className="hero__title">SKETCH LUXURY INTERIOR DESIGN</h1>
         <p className="hero__paragraph">Bring your dream to life with one-on-one design help & hand picked products tailored to your style, space and budget.</p>
         <button className="hero__btn">
